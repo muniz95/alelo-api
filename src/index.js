20367a1b3bc557362c4434c91b1cd24893687ad1
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2017-03-20 19:48:17 -0300

    request-promise adicionado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let request = require('request');
+let request = require('request-promise');
 const URL = "https://www.meualelo.com.br/meualelo.services/rest";
 
 request = request.defaults({jar: true});
@@ -9,6 +9,7 @@ class Alelo {
   constructor(cpf,pwd) {
     this.cpf = cpf;
     this.pwd = new Buffer(pwd).toString("base64");
+    this.jar = request.jar();
   }
 
   obterSaldo() {};
@@ -31,18 +32,22 @@ class Alelo {
         "Content-Type": "application/json"
       }
     };
-    request.post(opts, (err, httpResponse, body) => {
-      if(err) console.error(err);
-      console.log(body);
+    request.post(opts)
+    .then((response) => {
+      console.log(response);
+      // this.jar.setCookie(httpResponse.headers["set-cookie"], URL);
+    })
+    .catch((err) => {
+      if(err) console.error(`Erro: ${err}`);
     });
   };
 
   obterCartoes() {
-    // this.login();
-    // console.log(cookieJar);
-    request.get(`${URL}/user/card/preference/list`, (err, httpResponse, data) => {
-      console.log(httpResponse);
-    });
+    this.login();
+    // console.log(this.jar);
+    // request.get({url: `${URL}/user/card/preference/list`, jar: this.jar}, (err, httpResponse, data) => {
+    //   console.log(httpResponse.headers["set-cookie"]);
+    // });
   };
 }
 
