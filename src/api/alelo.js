a29620634638d47cb89571186b40b0160d96aa95
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2017-03-19 23:12:30 -0300

    Método de login em progresso

--- src/api/alelo.js
+++ src/api/alelo.js
@@ -1,7 +1,35 @@
-const axios = require('axios');
+"use strict";
 
+const axios = require('axios');
 const URL = "https://www.meualelo.com.br/meualelo.services/rest";
 
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
 const obterSaldo = () => {};
-const login = (cpf, pwd) => {};
+
+/**
+ * Efetua login
+ * @param {string} cpf
+ * @param {string} pwd
+ * @return {string}
+ */
+const login = (cpf, pwd) => {
+  axios({
+    url: `${URL}/login/authenticate`,
+    method: "post",
+    data: {
+      cpf: cpf,
+      pwd: pwd,
+      captchaResponse: ""
+    }
+  })
+  .then(response => console.log(response));
+};
 const obterCartoes = () => {};
+
+module.exports = {
+  obterSaldo: obterSaldo,
+  obterCartoes: obterCartoes,
+  login: login,
+  teste: ""
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,53 @@
-'use strict';
+"use strict";
 
-/**
- * Adds commas to a number
- * @param {number} number
- * @param {string} locale
- * @return {string}
- */
-module.exports = (number, locale) => {
-  return number.toLocaleString(locale);
-};
+const util = require('util');
+let request = require('request');
+const URL = "https://www.meualelo.com.br/meualelo.services/rest";
+
+request = request.defaults({jar: true});
+
+class Alelo {
+  constructor(cpf,pwd) {
+    this.cpf = cpf;
+    this.pwd = new Buffer(pwd).toString("base64");
+  }
+
+  obterSaldo() {};
+
+  /**
+   * Efetua login
+   * @param {string} cpf
+   * @param {string} pwd
+   * @return {string}
+   */
+  login() {
+    var opts = {
+      url: `${URL}/login/authenticate`,
+      formData: {
+        cpf: this.cpf,
+        pwd: this.pwd,
+        captchaResponse: ""
+      },
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+    request.post(opts, (err, httpResponse, body) => {
+      if(err) console.error(err);
+      console.log(body);
+    });
+  };
+
+  obterCartoes() {
+    this.login();
+    // console.log(cookieJar);
+    // axios.get(`${URL}/user/card/preference/list`, {
+    //   jar: cookieJar,
+    //   withCredentials: true
+    // })
+    // .then((response) => console.log(response))
+    // .catch((err) => console.error(`Erro: ${err}`));
+  };
+}
+
+export default Alelo
