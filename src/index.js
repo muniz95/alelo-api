addb2228e1df7652cf462b2393e53ed317d4b4cf
Rodrigo Muniz <muniz95@bsd.com.br>
2017-03-24 20:00:16 +0000

    Métodos para obter cartões e saldo de cartão implementados

--- src/index.js
+++ src/index.js
@@ -6,22 +6,27 @@ const URL = "https://www.meualelo.com.br/meualelo.services/rest";
 request = request.defaults({jar: true});
 
 class Alelo {
+  
+  /**
+   * Creates a new Alelo account instance.
+   * 
+   * @param {string} cpf - ID of the cardholder
+   * @param {string} pwd - Account password
+   *
+   */
   constructor(cpf,pwd) {
     this.cpf = cpf;
     this.pwd = new Buffer(pwd).toString("base64");
-    this.jar = request.jar();
+    this.jar = request.jar().setCookie("", "");
   }
-
-  obterSaldo() {};
-
+  
   /**
-   * Efetua login
-   * @param {string} cpf
-   * @param {string} pwd
-   * @return {string}
+   * Performs login on the system
+   * 
+   * @return {Promise}
    */
   login() {
-    var opts = {
+    const opts = {
       url: `${URL}/login/authenticate`,
       json: {
         cpf: this.cpf,
@@ -30,25 +35,40 @@ class Alelo {
       },
       headers: {
         "Content-Type": "application/json"
-      }
+      },
+      jar: this.jar
     };
-    request.post(opts)
-    .then((response) => {
-      console.log(response);
-      // this.jar.setCookie(httpResponse.headers["set-cookie"], URL);
-    })
-    .catch((err) => {
-      if(err) console.error(`Erro: ${err}`);
-    });
+    return request.post(opts)
+      .catch((err) => {
+        if(err) console.error(`Erro: ${err}`);
+      });
+  };
+   
+  /**
+   * Gets the card's balance, along with other info.
+   *
+   * @param {string} cardId - The card unique identification
+   * 
+   * @return {Promise<Object>}
+   */
+  getBalance(cardId) {
+    const opts = {
+      url: `${URL}/user/card/balance?selectedCardNumberId=${cardId}`
+    };
+    return request.get(opts)
+      .then(response => JSON.parse(response))
+      .catch((err) => console.error(err));
   };
 
-  obterCartoes() {
-    this.login();
-    // console.log(this.jar);
-    // request.get({url: `${URL}/user/card/preference/list`, jar: this.jar}, (err, httpResponse, data) => {
-    //   console.log(httpResponse.headers["set-cookie"]);
-    // });
+  getCards() {
+    const opts = {
+      url: `${URL}/user/card/preference/list`,
+      jar: this.jar
+    };
+    return request.get(opts)
+      .then(response => JSON.parse(response))
+      .catch((err) => console.error(err));
   };
 }
 
-export default Alelo
+module.exports = Alelo
